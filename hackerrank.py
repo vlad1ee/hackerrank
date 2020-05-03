d4a4082994948f06ec998a133664b908430fe038
vladl01 <vladlee.2001@mail.ru>
2020-05-03 17:11:35 +0600

    add file

---
+++ hackerrank.py
@@ -0,0 +1,10 @@
+# def hackerrankInString(s):
+#     word = 'hackerrank'
+#     a = 0
+#     for i in s:
+#         if i == word[a]:
+#             a+=1
+#         if a == len(word):
+#             return 'YES'
+#             break
+#     return 'NO'
